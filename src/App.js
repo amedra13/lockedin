98c0a9f39070328b314a13418a0196a90af43890
Andres Medrano <andresthedev5@gmail.com>
2022-02-19 01:24:32 -0800

    Side bar set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello locked in!</h1>
       <h3>Lets go JA!</h3>
+      <Sidebar/>
     </div>
   );
 }
